994df0cd6767862e3bef2b7710526fc4a2761c6c
Iris <iris.jennis@gmail.com>
2023-12-17 11:27:57 -0800

    Setup Game Engine

--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { PureComponent } from "react";
+import { GameEngine } from "react-game-engine";
+import { Box } from "./renderers";
+import { MoveBox } from "./systems"
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default class SimpleGame extends PureComponent {
+  render() {
+    return (
+      <GameEngine
+        style={{ width: 1920, height: 1080, backgroundColor: "#03e3fc" }}
+        systems={[MoveBox]}
+        entities={{
+          //-- Notice that each entity has a unique id (required)
+          //-- and a renderer property (optional). If no renderer
+          //-- is supplied with the entity - it won't get displayed.
+          box1: { x: 200,  y: 200, renderer: <Box />},
+          box2: { x: 250,  y: 250, renderer: <Box />},
+          box3: { x: 250,  y: 250, renderer: <Box />}
+        }}>
+
+      </GameEngine>
+    );
+  }
 }
-
-export default App;
---
+++ src/renderers.js
@@ -0,0 +1,21 @@
+import React, { Component } from "react";
+
+
+class Character extends Component{
+  render(){
+
+  }
+}
+
+class Box extends Component {
+  render() {
+    const size = 100;
+    const x = this.props.x - size / 2;
+    const y = this.props.y - size / 2;
+    return (
+      <div style={{ position: "absolute", width: size, height: size, backgroundColor: "blue", left: x, top: y }} />
+    );
+  }
+}
+
+export { Box };
