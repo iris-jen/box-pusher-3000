09f3e6f128b9a45b9aee2e9e3026c38aec32dbd1
Iris <iris.jennis@gmail.com>
2023-12-17 22:01:33 -0800

    cleaning

--- src/App.js
+++ src/App.js
@@ -1,9 +1,7 @@
+import React, { useEffect, useState } from "react";
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Container from 'react-bootstrap/Container';
-import Card from 'react-bootstrap/Card';
 import Navbar from 'react-bootstrap/Navbar';
-
-import React, { useEffect, useState } from "react";
 import { GameEngine } from "react-game-engine";
 import { Player, Hole, Box } from "./renderers";
 import { MovePlayer } from "./systems";
@@ -30,27 +28,20 @@ export default function SimpleGame() {
 
   return (
     <div>
-          <Navbar expand="lg" className="bg-body-tertiary">
-            <Container>
-              <Navbar.Brand href="#home">💥💥💥💥Box Pusher 30000000💥💥💥💥</Navbar.Brand>
-              <Card/>
-            </Container>
-          </Navbar>
+      <Navbar expand="lg" className="bg-body-tertiary">
+        <Container>
+          <Navbar.Brand href="#home">💥💥💥💥Box Pusher 30000000💥💥💥💥</Navbar.Brand>
+        </Container>
+      </Navbar>
 
-          <GameEngine
-            style={{
-              width: screenSize.width,
-              height: screenSize.height,
-              backgroundColor: "pink",
-            }}
-            systems={[MovePlayer]}
-            entities={{
-              box: {x:400, y:400, renderer: <Box/>},
-              player: { x: 100, y: 100, renderer: <Player /> },
-              hole: {x: 600, y: 600, renderer: <Hole/>}
-            }}
-          />
-    </div>
-    
+      <GameEngine style={{ width: screenSize.width,
+                            height: screenSize.height,
+                            backgroundColor: "pink" }}
+        systems={[MovePlayer]}
+        entities={{
+          box: {x:400, y:400, renderer: <Box/>},
+          player: { x: 100, y: 100, renderer: <Player /> },
+          hole: {x: 600, y: 600, renderer: <Hole/>}}}/>
+    </div>   
   );
 }
